4690421b94e0f6b575513fdb29618d8ae1dce70b
zeyti12a <116185849+zeyti12a@users.noreply.github.com>
2023-09-26 21:20:12 -0300

    Livros por Categoria

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -4,3 +4,13 @@ BEGIN
     SELECT * FROM Autor;
 END //
 DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
