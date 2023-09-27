696c2f6735eb26d3a9e92de8ba2750e18782e6b0
zeyti12a <116185849+zeyti12a@users.noreply.github.com>
2023-09-26 21:30:10 -0300

    Livros e Seus Autores

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -98,3 +98,9 @@ BEGIN
 END //
 DELIMITER ;
 
+   SELECT Livro.Titulo, CONCAT(Autor.Nome, ' ', Autor.Sobrenome) AS Autor
+    FROM Livro
+    INNER JOIN Autor_Livro ON Livro.Livro_ID = Autor_Livro.Livro_ID
+    INNER JOIN Autor ON Autor_Livro.Autor_ID = Autor.Autor_ID;
+END //
+DELIMITER ;
