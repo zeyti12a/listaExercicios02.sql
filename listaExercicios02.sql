da1411a9f89e0a2fdc82881a9c633c2aa8444ef3
zeyti12a <116185849+zeyti12a@users.noreply.github.com>
2023-09-26 21:20:59 -0300

    Contagem de Livros por Categoria

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -14,3 +14,13 @@ BEGIN
     WHERE Categoria.Nome = categoriaNome;
 END //
 DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT COUNT(Livro.Titulo) AS TotalLivros
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END //
+DELIMITER ;
